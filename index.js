429659996d43fd108a961d5c9f0fadade06d14da
Porter Libby <pelibby16@earlham.edu>
2017-07-06 23:50:22 -0400

    Add 1 new sound + *serious* style changes

--- index.js
+++ index.js
@@ -8,8 +8,29 @@ function clickHandle(n) {
         twammichCounter()
     }
 }
+
 function twammichCounter() {
     t++
     console.log(t)
     document.getElementById("tCount").textContent=`🍔 twAMMICh_cOUNteRR: ${t}`;
+}
+
+function switchSet(n,x) {
+    console.log(`${n}`)
+    document.getElementById("cat1").style.display = 'none'
+    document.getElementById("cat2").style.display = 'none'
+    document.getElementById("cat3").style.display = 'none'
+    document.getElementById("cat4").style.display = 'none'
+    document.getElementById("cat5").style.display = 'none'
+
+    document.getElementById(`${n}`).style.display = 'block'
+
+    document.getElementById("n1").style.color = 'black'
+    document.getElementById("n2").style.color = 'black'
+    document.getElementById("n3").style.color = 'black'
+    document.getElementById("n4").style.color = 'black'
+    document.getElementById("n5").style.color = 'black'
+
+    document.getElementById(`${x}`).style.color = 'red'
+
 }
